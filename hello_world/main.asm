a8a517ca8131d5dfd86c32565c8f883349d2ce2e
M Lau <63963445+tofuuudon@users.noreply.github.com>
2025-02-08 16:59:29 +0000

    refactor: code format

--- hello_world/main.asm
+++ hello_world/main.asm
@@ -1,17 +1,19 @@
 section .data
-	msg 	db 'hello, world', 10
-	msglen 	equ $-msg
+	msg db 'hello, world', 10
+	msglen equ $-msg
 
 section .text
-		global	_start
-	_start:
-		mov 	rdi, 1
-		mov 	rsi, msg
-		mov	rdx, msglen
-		mov 	rax, 1
-		syscall
+	global _start
 
-		mov 	rdi, 0
-		mov	rax, 60
-		syscall
+_start:
+	; prints 'hello, world'
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, msglen
+	mov rax, 1
+	syscall
 
+	; exits 0
+	mov rdi, 0
+	mov rax, 60
+	syscall
